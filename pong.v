c5908bd51ad2373ca15632501245bfbe11120d78
Michaeldanielsss <daniels@bu.edu>
2024-12-04 14:30:47 -0500

    Update pong.v

--- pong.v
+++ pong.v
@@ -10,13 +10,13 @@ module pong(
     output [11:0] rgb       // to DAC, to VGA port
     );
     
-    wire w_reset, w_up, w_down, w_vid_on, w_p_tick;
+    wire w_reset, w_up, w_down, w_vid_on, w_pixel;
     wire [9:0] w_x, w_y;
     reg [11:0] rgb_reg;
     wire [11:0] rgb_next;
     
     vga_controller vga(.clk(clk), .reset(w_reset), .video_on(w_vid_on),
-                       .hsync(hsync), .vsync(vsync), .p_tick(w_p_tick), .x(w_x), .y(w_y));
+                       .hsync(hsync), .vsync(vsync), .p_tick(w_pixel), .x(w_x), .y(w_y));
     pixel pix(.clk(clk), .reset(w_reset), .up(w_up), .down(w_down), 
                  .video_on(w_vid_on), .x(w_x), .y(w_y), .rgb(rgb_next));
     debouncer dbright(.clk(clk), .button(reset), .clean(w_reset));
