924c26a894491bd6b8e49614551423bca49c0a75
Michaeldanielsss <daniels@bu.edu>
2024-12-05 13:50:38 -0500

    pong.v

--- pong.v
+++ pong.v
@@ -1,10 +1,10 @@
 `timescale 1ns / 1ps
 
 module pong(
-    input clk,       // from Basys 3
-    input reset,            // btnR
-    input up,               // btnU
-    input down,             // btnD
+    input clk,       
+    input reset,            // btnright
+    input up,               // btnup
+    input down,             // btndown
     output hsync,           // to VGA port
     output vsync,           // to VGA port
     output [11:0] rgb       // to DAC, to VGA port
@@ -12,21 +12,21 @@ module pong(
     
     wire w_reset, w_up, w_down, w_vid_on, w_pixel;
     wire [9:0] w_x, w_y;
-    reg [11:0] rgb_reg;
-    wire [11:0] rgb_next;
+    reg [11:0] reg_rgb;
+    wire [11:0] w_rgb;
     
     vga_controller vga(.clk(clk), .reset(w_reset), .video_on(w_vid_on),
                        .hsync(hsync), .vsync(vsync), .p_pixel(w_pixel), .x(w_x), .y(w_y));
     pixel pix(.clk(clk), .reset(w_reset), .up(w_up), .down(w_down), 
-                 .video_on(w_vid_on), .x(w_x), .y(w_y), .rgb(rgb_next));
+              .video_on(w_vid_on), .x(w_x), .y(w_y), .rgb(w_rgb));
     debouncer dbright(.clk(clk), .button(reset), .clean(w_reset));
     debouncer dbup(.clk(clk), .button(up), .clean(w_up));
     debouncer dbdown(.clk(clk), .button(down), .clean(w_down));
     
     always @(posedge clk)
-        if(w_p_tick)
-            rgb_reg <= rgb_next;
+        if(w_pixel)
+            reg_rgb <= w_rgb;
             
-    assign rgb = rgb_reg;
+    assign rgb = reg_rgb;
     
 endmodule
