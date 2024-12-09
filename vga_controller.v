329685b097a926745be229a9d4e1916849a1aec6
Prashastp25 <86373449+Prashastp25@users.noreply.github.com>
2024-12-09 18:13:45 -0500

    Update vga_controller.v

--- vga_controller.v
+++ vga_controller.v
@@ -13,17 +13,17 @@ module vga_controller(
     
     // Based on VGA standards found at vesa.org for 640x480 resolution
     // Total horizontal width of screen = 800 pixels, partitioned  into sections
-    parameter HD = 640;             // horizontal display area width 
-    parameter HF = 48;              // horizontal front porch width 
-    parameter HB = 16;              // horizontal back porch width 
-    parameter HR = 96;              // horizontal retrace width 
-    parameter HMAX = HD+HF+HB+HR-1; // max value of horizontal counter = 799
+    parameter h_display = 640;             // horizontal display area width 
+    parameter h_front = 48;              // horizontal front porch width 
+    parameter h_back = 16;              // horizontal back porch width 
+    parameter h_retrace = 96;              // horizontal retrace width 
+    parameter h_max = h_display + h_front + h_back + h_retrace - 1; // max value of horizontal counter = 799
     // Total vertical length of screen = 525 pixels, partitioned into sections
-    parameter VD = 480;             // vertical display area length 
-    parameter VF = 10;              // vertical front porch length  
-    parameter VB = 33;              // vertical back porch length  
-    parameter VR = 2;               // vertical retrace length   
-    parameter VMAX = VD+VF+VB+VR-1; // max value of vertical counter = 524   
+    parameter v_display = 480;             // vertical display area length 
+    parameter v_front = 10;              // vertical front porch length  
+    parameter v_back = 33;              // vertical back porch length  
+    parameter v_retrace = 2;               // vertical retrace length   
+    parameter v_max = v_display + v_front + v_back + v_retrace - 1; // max value of vertical counter = 524   
     
 	reg  [1:0] r_25MHz;
 	wire w_25MHz;
@@ -64,7 +64,7 @@ module vga_controller(
         if(reset)
             h_count_next = 0;
         else
-            if(h_count_reg == HMAX)                 // end of horizontal scan
+            if(h_count_reg == h_max)                 // end of horizontal scan
                 h_count_next = 0;
             else
                 h_count_next = h_count_reg + 1;         
@@ -74,20 +74,20 @@ module vga_controller(
         if(reset)
             v_count_next = 0;
         else
-            if(h_count_reg == HMAX)                 // end of horizontal scan
-                if((v_count_reg == VMAX))           // end of vertical scan
+            if(h_count_reg == h_max)                 // end of horizontal scan
+                if((v_count_reg == v_max))           // end of vertical scan
                     v_count_next = 0;
                 else
                     v_count_next = v_count_reg + 1;
         
     // h_sync_next asserted within the horizontal retrace area
-    assign h_sync_next = (h_count_reg >= (HD+HB) && h_count_reg <= (HD+HB+HR-1));
+    assign h_sync_next = (h_count_reg >= (h_display+h_back) && h_count_reg <= (h_display+h_back+h_retrace-1));
     
     // v_sync_next asserted within the vertical retrace area
-    assign v_sync_next = (v_count_reg >= (VD+VB) && v_count_reg <= (VD+VB+VR-1));
+    assign v_sync_next = (v_count_reg >= (v_display+v_back) && v_count_reg <= (v_display+v_back+v_retrace-1));
     
     // Video ON/OFF - only ON while pixel counts are within the display area
-    assign video_on = (h_count_reg < HD) && (v_count_reg < VD); // 0-639 and 0-479 
+    assign video_on = (h_count_reg < h_display) && (v_count_reg < v_display); // 0-639 and 0-479 
             
     // Outputs
     assign hsync  = h_sync_reg;
